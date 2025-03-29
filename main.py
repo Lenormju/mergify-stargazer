f4e9c3437b25f4335d4f57da538f4f2aa00373f8
Lenormju <lenormand.julien0@gmail.com>
2025-03-29 09:02:39 +0100

    doc: GitHub API usage

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from mergify-stargazer!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/stargazer_service.py
@@ -0,0 +1,25 @@
+import os
+
+# see https://docs.github.com/en/rest/using-the-rest-api/getting-started-with-the-rest-api?apiVersion=2022-11-28
+# rate limit, cf https://docs.github.com/en/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28
+# - primary :
+#   - if unauthenticated : 60 calls per hour
+#   - if authenticated : 5000 calls per hour (when not being a GitHub App)
+# - secondary :
+#   - 100 max concurrent requests
+#   - 900 calls per minute on a single endpoint
+#   - ... and others
+# https://docs.github.com/en/rest/using-the-rest-api/best-practices-for-using-the-rest-api?apiVersion=2022-11-28
+# if rate limited :
+# - check for "retry-after" response header
+# - check for "x-ratelimit-remaining" response header being 0, then "x-ratelimit-reset"
+# - wait exponential time
+# - avoid being banned by making the calls anyway
+# redirections : 301, 302, 307
+# condition requests (time/change-based)
+# https://docs.github.com/en/rest/using-the-rest-api/using-pagination-in-the-rest-api?apiVersion=2022-11-28
+# - check for "link" header : rel="next"
+# - "per_page" query parameter
+
+
+token = os.getenv("GITHUB_API_ACCESS_TOKEN")  # FIXME: store the secret another way
